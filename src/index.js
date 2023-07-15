26d1c3fd994449ddc88222027cb5020af508916a
Nick Blanco <nablanco92@gmail.com>
2023-07-15 16:39:15 +0200

    Remove attribution

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Component from "./components/Component/Component";
-import Attribution from "./components/Attribution/Attribution";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="app">
       <Component></Component>
-      <Attribution></Attribution>
     </div>
   </React.StrictMode>
 );
